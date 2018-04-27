2dfafebee865a04dc03e5cab8e6c227b93cd2cf1
nex79et <RAVIKUMAR.SAHU@pb.com>
2018-04-27 14:34:40 +0530

    removing SigninComponent component duplicacy

--- src/app/user/signin/signin.module.ts
+++ src/app/user/signin/signin.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SigninComponent } from './signin.component';
+// import { SigninComponent } from './signin.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SigninComponent]
+  declarations: []
 })
 export class SigninModule { }
